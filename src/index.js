ca8d013bf6163acfc55fb6f60e4f821366c72ba6
Jiří Čermák <jiri.cermak482@gmail.com>
2017-07-05 21:42:42 +0200

    replacing of ids (Number) with symbols (Symbol)

--- src/index.js
+++ src/index.js
@@ -1,115 +1,138 @@
+const listeners = new Map();
+
+function createHandler(sym, handler, onetime, detach) {
+    if (onetime) {
+        event => {
+            detach(sym);
+            handler(event);
+        };
+    }
+
+    return handler;
+}
+
 export default class DomEventHandler {
     constructor() {
-        this.id = 0;
-        this.listeners = new Map();
-        this.callbacks = new Map();
-    }
-
-    _createCallback(id, fn, onetime) {
-        if (onetime) {
-            this.callbacks.set(id, event => {
-                this.detach(id);
-
-                fn(event);
-            });
-        } else {
-            this.callbacks.set(id, fn);
-        }
-    }
-
-    /*
-        @param {HTMLElement} element
-        @param {String} event
-        @param {Function} fn
-        @param {Boolean} capture
-        @param {Boolean} onetime
-        @return {Number} id
-    */
-    _attach(element, eventName, callback, onetime, capture) {
-        const id = this.id;
-
-        this._createCallback(id, callback, onetime);
-
-        // save data
-        this.listeners.set(id, { element, eventName, onetime, capture });
-
-        // add event listener
-        element.addEventListener(eventName, this.callbacks.get(id), capture);
-
-        return this.id++;
+        this.detach = this.detach.bind(this);
     }
 
     /**
-     * attach new event handler
-     * @param  {HTMLElement or NodeList}   element   [description]
-     * @param  {String}   eventName [description]
-     * @param  {Function} callback  [description]
-     * @param  {Boolean}   onetime   should auto. detach after execution
-     * @param  {Boolean}   capture   [description]
-     * @return {Number}             event handler id or array of ids
-     */
-    attach(element, eventName, callback, onetime, capture) {
-        if (!element.length) {
-            return this._attach(element, eventName, callback, onetime, capture);
+	    @param {HTMLElement} element
+	    @param {String} eventName event name
+	    @param {Function} handler event handler
+	    @param {Boolean} capture
+	    @param {Boolean} onetime
+	    @return {Symbol}
+	*/
+    _attach({ target, name, handler, capture = false }, onetime = false) {
+        const sym = Symbol();
+
+        const eventData = {
+            handler: createHandler(sym, handler, onetime, this.detach),
+            target: target,
+            name: name,
+            capture
+        };
+
+        // save data
+        listeners.set(sym, {
+            event: eventData,
+            onetime
+        });
+
+        // set event listener
+        target.addEventListener(name, eventData.handler, capture);
+
+        return sym;
+    }
+
+    /**
+	 * attach new event handler
+	 * @param  {HTMLElement or NodeList}   eventTarget
+	 * @param  {String}   eventName
+	 * @param  {Function} eventHandler
+	 * @param  {Boolean}   capture
+	 * @param  {Boolean}   onetime   should auto. detach after execution
+	 * @return {Symbol or Array of Symbols} event handler id or array of ids
+	 */
+    attach(
+        eventTarget,
+        eventName,
+        eventHandler,
+        capture = false,
+        onetime = false
+    ) {
+        if (!eventTarget.length) {
+            return this._attach(
+                eventTarget,
+                eventName,
+                eventHandler,
+                capture,
+                onetime
+            );
         }
 
-        const elements = [...element];
-        const id = [];
+        const targets = Array.from(eventTarget); // create array from NodeList
+        const symbols = [];
 
-        for (let el of elements) {
-            id.push(this._attach(el, eventName, callback, onetime, capture));
+        for (const target of targets) {
+            symbols.push(
+                this._attach(target, eventName, eventHandler, capture, onetime)
+            );
         }
 
-        return id.length === 1 ? id[0] : id;
+        return symbols;
     }
 
     /*
-        @param {Number} id
-        @return {Boolean}
-    */
-    _detach(key, value) {
-        const data = value ? value : this.listeners.get(key);
+	    @param {Number} id
+	    @return {Boolean}
+	*/
+    // _detach(key, value) {
+    //     const data = value ? value : this.listeners.get(key);
+    //
+    //     if (data === undefined) return false;
+    //
+    //     const callback = this.callbacks.get(key);
+    //
+    //     this.callbacks.delete(key);
+    //
+    //     data.element.removeEventListener(
+    //         data.eventName,
+    //         callback,
+    //         data.capture
+    //     );
+    //
+    //     return this.listeners.delete(key);
+    // }
 
-        if (data === undefined) return false;
+    detach(symbol) {}
 
-        const callback = this.callbacks.get(key);
-
-        this.callbacks.delete(key);
-
-        data.element.removeEventListener(
-            data.eventName,
-            callback,
-            data.capture
-        );
-
-        return this.listeners.delete(key);
-    }
-
-    detach(id) {
-        if (typeof id !== "number" && !Array.isArray(id)) {
-            throw "Argument must be number or array of numbers. An id/s of event listener/s returned by the attach method.";
-        }
-
-        const results = [];
-
-        if (!id.length) id = [id];
-
-        for (let num of id) {
-            results.push(this._detach(num));
-        }
-
-        return results.length === 1 ? results[0] : results;
-    }
-
-    destroy() {
-        this.listeners.forEach((data, id) => {
-            this._detach(id, data);
-        });
-
-        this.listeners.clear();
-
-        this.callbacks.clear();
-
-        this.id = 0;
-    }
+    // detach(id) {
+    //     if (typeof id !== "number" && !Array.isArray(id)) {
+    //         throw "Argument must be number or array of numbers. An id/s of event listener/s returned by the attach method.";
+    //     }
+    //
+    //     const results = [];
+    //
+    //     if (!id.length) id = [id];
+    //
+    //     for (let num of id) {
+    //         results.push(this._detach(num));
+    //     }
+    //
+    //     return results.length === 1 ? results[0] : results;
+    // }
+    //
+    // destroy() {
+    //     this.listeners.forEach((data, id) => {
+    //         this._detach(id, data);
+    //     });
+    //
+    //     this.listeners.clear();
+    //
+    //     this.callbacks.clear();
+    //
+    //     this.id = 0;
+    // }
 }
